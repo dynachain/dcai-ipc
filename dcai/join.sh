bfcc047db89fcd008f6532ab6ae3c9c6803d9344
kahwai <chooikw@gmail.com>
2025-02-11 23:39:10 +0800

    git merge sanderpick/sander/fix-gateway-genesis-validators

---
+++ dcai/join.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#cargo run -q -p ipc-cli --release --  subnet join --from=0x5b34687030fb33a0d0ae91209c6c95708f520904 --collateral 100  --subnet=/r314159/t410fz57pceba4tlxqceax7r3hnm5ezm5kqmydbaeeky  --initial-balance 100
+
+#cargo run -q -p ipc-cli --release --  subnet join --from=0x7fe4212c2cce8f41e01b9a74659d42f742c251d1 --collateral 100  --subnet=/r314159/t410fz57pceba4tlxqceax7r3hnm5ezm5kqmydbaeeky 
+
+#cargo run -q -p ipc-cli --release --  subnet join --from=0x1dd205d0abf1bef3e7b8efceeb30881e4f4ce3b6 --collateral 100  --subnet=/r314159/t410fz57pceba4tlxqceax7r3hnm5ezm5kqmydbaeeky
+
+#cargo run -q -p ipc-cli --release --  subnet join --from=0xe751dcce6ea51e9dfecb225bcc92cc142767d501 --collateral 100  --subnet=/r314159/t410fz57pceba4tlxqceax7r3hnm5ezm5kqmydbaeeky --initial-balance 100
+
+ipc-cli subnet join --from=0x507baa1945a09df8d6b3a2e712c413ef02d26fe9 --collateral 100  --subnet=/r314159/t410fe7mk2dglpf4zkr3dnabam3o37ei3rujcaixkcra --initial-balance 100
+ipc-cli subnet join --from=0x7fe4212c2cce8f41e01b9a74659d42f742c251d1 --collateral 100  --subnet=/r314159/t410fe7mk2dglpf4zkr3dnabam3o37ei3rujcaixkcra --initial-balance 100
+ipc-cli subnet join --from=0x1dd205d0abf1bef3e7b8efceeb30881e4f4ce3b6 --collateral 100  --subnet=/r314159/t410fe7mk2dglpf4zkr3dnabam3o37ei3rujcaixkcra --initial-balance 100
